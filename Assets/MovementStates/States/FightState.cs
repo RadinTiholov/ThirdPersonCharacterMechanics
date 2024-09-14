2817da20279b9c299df3446d070da133a27b7acc
Radin Tiholov <radin.tiholov@mail.bg>
2024-09-14 21:48:58 +0300

    Add attack animation

--- Assets/MovementStates/States/FightState.cs
+++ Assets/MovementStates/States/FightState.cs
@@ -1,12 +1,16 @@
+using UnityEditor;
+using UnityEngine;
 public class FightState : MovementBaseState
 {
     public override void EnterState(MovementStateManager movement)
     {
+        // Set the "Fighting" animation state to true
         movement.anim.SetBool("Fighting", true);
     }
 
     public override void UpdateState(MovementStateManager movement)
     {
+        // Set movement speed based on the vertical input
         if (movement.vInput < 0)
         {
             movement.currentMoveSpeed = movement.fightBackSpeed;
@@ -15,11 +19,32 @@ public class FightState : MovementBaseState
         {
             movement.currentMoveSpeed = movement.fightSpeed;
         }
+
+        // When the mouse button is pressed (attack starts)
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Set "Attacking" to true
+            movement.anim.SetBool("Attacking", true);
+        }
+
+        // When the mouse button is released (attack ends)
+        if (Input.GetMouseButtonUp(0))
+        {
+            // Set "Attacking" to false
+            movement.anim.SetBool("Attacking", false);
+        }
     }
 
-    void ExitState(MovementStateManager movement, MovementBaseState state)
+
+    public void ExitState(MovementStateManager movement, MovementBaseState state)
     {
+        // Reset the "Fighting" animation state to false when exiting
         movement.anim.SetBool("Fighting", false);
+
+        // Reset the "Attacking" state to false when exiting
+        movement.anim.SetBool("Attacking", false);
+
+        // Switch to the new state
         movement.SwitchState(state);
     }
 }
